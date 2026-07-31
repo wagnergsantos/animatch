15aab747435747b925bd45ea138e6005217b8f0d
wagnergsantos <wagner.gsantos@gmail.com>
2026-07-31 19:16:42 -0300

    Updates

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'animatch-v1'
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS)
+    }).then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    }).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || !event.request.url.startsWith('http')) {
+    return
+  }
+
+  // Network-first strategy for API calls, Cache-first for static assets
+  if (event.request.url.includes('graphql.anilist.co')) {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse.ok) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse))
+          }
+        }).catch(() => {})
+        return cachedResponse
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+        const responseToCache = response.clone()
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache)
+        })
+        return response
+      })
+    })
+  )
+})
